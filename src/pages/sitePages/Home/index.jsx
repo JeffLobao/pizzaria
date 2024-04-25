be92ceeda93155a110c19eb88becfa4da2c81282
Jefflobao <jefferson_lobao@hotmail.com>
2024-04-25 11:59:30 -0300

    api

--- src/pages/sitePages/Home/index.jsx
+++ src/pages/sitePages/Home/index.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+   
+    const [pizzas, setPizzas] = useState([]);
+    
+    async function buscarSabores(){
+        const request = await fetch("http://localhost:3000/sabores");
+        const response = await request.json();
+        setPizzas(response);
+    }
+
+    useEffect(() => {
+        buscarSabores();
+    }, [])
+    
     return(
         <>
-        home
+        <h1>Pizzas em destaque</h1>
+        {
+            pizzas && pizzas.map(p => (
+                <div>
+                    {p.nome}
+                </div>
+            ))
+        }
         </>
     );
 }
